a02d238195b3db0fefcf3eed3a690f1a23acead0
Sergio Umana <sumana@sumana-mac.local>
2015-11-17 09:33:29 -0800

    range fix

--- helpers/stringHelper.go
+++ helpers/stringHelper.go
@@ -7,7 +7,7 @@ import (
 func StringConcat(strings ...string) string {
 
 	var buffer bytes.Buffer
-	for i, v := range strings {
+	for _, v := range strings {
 		buffer.WriteString(v)
 	}
 	return buffer.String()
